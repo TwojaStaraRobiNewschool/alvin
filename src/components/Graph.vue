37dfebc98eea7b4251dd5b15a4224ff8d450f1ee
Maciej Augustyniak <maciej.f.august@gmail.com>
2019-05-22 17:55:07 +0200

    Refactored graph component

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,39 +1,13 @@
 <template>
   <div>
-    <v-stage
-      @mousemove="handleStageMouseMove"
-      :config="{ width, height }"
-    >
+    <v-stage :config="{ width, height }">
       <v-layer>
-        <v-rect
+        <graph-rect
           v-for="(value, index) in array"
           :key="index"
-          :config="{
-            x: index * rectWidth,
-            y: height - height / biggestValue * value,
-            height: height / biggestValue * value,
-            width: rectWidth,
-            fill: getRectColor(index)
-          }"
-          @mouseenter="() => handleRectMouseEnter(index)"
-          @mouseleave="() => handleRectMouseLeave(index)"
-        />
-        <v-rect
-          v-if="hoveredRect >= 0"
-          :config="{
-            ...rectInfoConfig,
-            fill: 'black',
-            stroke: 'white'
-          }"
-        />
-        <v-text
-          v-if="hoveredRect >= 0"
-          :config="{
-            ...textInfoConfig,
-            text: array[hoveredRect],
-            fontSize: 15,
-            fill: 'white'
-          }"
+          :index="index"
+          :maxHeight="height"
+          :maxWidth="width"
         />
       </v-layer>
     </v-stage>
@@ -41,97 +15,37 @@
 </template>
 
 <script>
+import GraphRect from './GraphRect'
 import { mapState } from 'vuex'
 
 export default {
+  components: {
+    GraphRect
+  },
+
   data () {
     return {
       height: 0,
-      width: 0,
-      hoveredRect: -1,
-      rectInfoConfig: {
-        x: 0,
-        y: 0,
-        width: 60,
-        height: 30
-      },
-      textInfoConfig: {
-        x: 0,
-        y: 0
-      }
+      width: 0
     }
   },
-  computed: {
-    ...mapState(['array']),
-    rectWidth () {
-      return this.width / this.array.length
-    },
-    biggestValue () {
-      let biggest = 0
-      this.array.forEach((value, index) => {
-        if (value > this.array[biggest]) {
-          biggest = index
-        }
-      })
-      return this.array[biggest]
-    }
+
+  computed: mapState(['array']),
+
+  mounted () {
+    window.addEventListener('resize', this.handleWindowResize)
+    this.handleWindowResize()
   },
-  props: {
-    comparing: Array,
-    swapping: Array
+
+  destroyed () {
+    window.removeEventListener('resize', this.handleWindowResize)
   },
+
   methods: {
-    getRectColor (index) {
-      if (this.hoveredRect === index) {
-        return '#ddd'
-      }
-
-      if (index in this.comparing) {
-        return 'green'
-      }
-
-      if (index in this.swapping) {
-        return 'red'
-      }
-
-      return 'black'
-    },
-    onResize () {
+    handleWindowResize () {
       this.width = this.$el.clientWidth
       this.height = this.$el.clientHeight
-    },
-    handleStageMouseMove (event) {
-      this.rectInfoConfig.y = event.evt.layerY + 10
-      this.rectInfoConfig.x = event.evt.layerX + 10
-      this.textInfoConfig.y = event.evt.layerY + 20
-      this.textInfoConfig.x = event.evt.layerX + 20
-
-      if (this.rectInfoConfig.x + this.rectInfoConfig.width > this.width) {
-        this.rectInfoConfig.x -= this.rectInfoConfig.width + 20
-        this.textInfoConfig.x -= this.rectInfoConfig.width + 20
-      }
-
-      if (this.rectInfoConfig.y + this.rectInfoConfig.height > this.height) {
-        this.rectInfoConfig.y -= this.rectInfoConfig.height + 20
-        this.textInfoConfig.y -= this.rectInfoConfig.height + 20
-      }
-    },
-    handleRectMouseEnter (index) {
-      this.hoveredRect = index
-    },
-    handleRectMouseLeave (index) {
-      this.hoveredRect = -1
-    },
-    handleArrayChange (event) {
-      console.log(event)
     }
-  },
-  mounted () {
-    window.addEventListener('resize', this.onResize)
-    this.onResize()
-  },
-  destroyed () {
-    window.removeEventListener('resize', this.onResize)
   }
 }
 </script>
---
+++ src/components/GraphRect.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-rect :config="config" />
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  props: {
+    index: {
+      type: Number,
+      requred: true
+    },
+
+    maxWidth: {
+      type: Number,
+      required: true
+    },
+
+    maxHeight: {
+      type: Number,
+      required: true
+    }
+  },
+
+  computed: {
+    ...mapState(['array']),
+
+    config () {
+      const width = this.maxWidth / this.array.length
+
+      return {
+        width,
+        x: this.index * width,
+        y: this.maxHeight - this.maxHeight / this.maxValue * this.value,
+        height: this.maxHeight / this.maxValue * this.value,
+        fill: '#000'
+      }
+    },
+
+    maxValue () {
+      let max = 0
+      this.array
+        .forEach((value, index) => {
+          if (value > this.array[max]) {
+            max = index
+          }
+        })
+      return this.array[max]
+    },
+
+    value () {
+      return this.array[this.index]
+    }
+  }
+}
+</script>
